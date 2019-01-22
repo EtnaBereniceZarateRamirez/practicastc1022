64c464065ac69532581a396734371c2e425ef796
EtnaBereniceZarateRamirez <42215120+EtnaBereniceZarateRamirez@users.noreply.github.com>
2019-01-21 21:57:26 -0600

    Add files via upload

---
+++ Persona.java
@@ -0,0 +1,15 @@
+public class Persona
+{
+    private int edad; 
+    private String name;
+    private char genero; 
+
+
+    public String getName() {
+    return name;
+}
+public void setName (String name)
+{
+    this.name = name;
+}
+}
